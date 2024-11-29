0c2115c002c37cdb1dda88f19e5ffe4c49cef756
hussein-mourad <38833034+Hussein-Mourad@users.noreply.github.com>
2024-11-29 08:51:56 +0200

    style: formatted code

--- esp8266-remote-pc-control.ino
+++ esp8266-remote-pc-control.ino
@@ -5,7 +5,7 @@
  *  https://mischianti.org/
  *
  */
- 
+
 #include "Arduino.h"
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
@@ -15,21 +15,21 @@
 
 #define HTTP_REST_PORT 8080
 ESP8266WebServer server(HTTP_REST_PORT);
- 
+
 // Serving Hello world
 void getHelloWord() {
-    server.send(200, "text/json", "{\"name\": \"Hello world\"}");
+  server.send(200, "text/json", "{\"name\": \"Hello world\"}");
 }
- 
+
 // Define routing
 void restServerRouting() {
-    server.on("/", HTTP_GET, []() {
-        server.send(200, F("text/html"),
-            F("Welcome to the ESP web server"));
-    });
-    server.on(F("/hello"), HTTP_GET, getHelloWord);
+  server.on("/", HTTP_GET, []() {
+    server.send(200, F("text/html"),
+                F("Welcome to the ESP web server"));
+  });
+  server.on(F("/hello"), HTTP_GET, getHelloWord);
 }
- 
+
 // Manage not found URL
 void handleNotFound() {
   String message = "File Not Found\n\n";
@@ -45,13 +45,13 @@ void handleNotFound() {
   }
   server.send(404, "text/plain", message);
 }
- 
+
 void setup(void) {
   Serial.begin(115200);
   WiFi.mode(WIFI_STA);
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
   Serial.println("");
- 
+
   // Wait for connection
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
@@ -62,13 +62,13 @@ void setup(void) {
   Serial.println(WIFI_SSID);
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
- 
+
   // Activate mDNS this is used to be able to connect to the server
   // with local DNS hostmane esp8266.local
   if (MDNS.begin("esp8266")) {
     Serial.println("MDNS responder started");
   }
- 
+
   // Set server routing
   restServerRouting();
   // Set not found response
@@ -77,7 +77,7 @@ void setup(void) {
   server.begin();
   Serial.println("HTTP server started");
 }
- 
+
 void loop(void) {
   server.handleClient();
 }
