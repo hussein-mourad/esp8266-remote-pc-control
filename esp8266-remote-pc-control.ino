047fae4066c6934d53b6a2d19dec1b493e436032
hussein-mourad <38833034+Hussein-Mourad@users.noreply.github.com>
2024-11-29 08:25:41 +0200

    Initial Commit

---
+++ esp8266-remote-pc-control.ino
@@ -0,0 +1,83 @@
+/*
+ *  Simple hello world Json REST response
+  *  by Mischianti Renzo <https://mischianti.org>
+ *
+ *  https://mischianti.org/
+ *
+ */
+ 
+#include "Arduino.h"
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266mDNS.h>
+#include "secrets.h"
+
+#define HTTP_REST_PORT 8080
+ESP8266WebServer server(HTTP_REST_PORT);
+ 
+// Serving Hello world
+void getHelloWord() {
+    server.send(200, "text/json", "{\"name\": \"Hello world\"}");
+}
+ 
+// Define routing
+void restServerRouting() {
+    server.on("/", HTTP_GET, []() {
+        server.send(200, F("text/html"),
+            F("Welcome to the ESP web server"));
+    });
+    server.on(F("/hello"), HTTP_GET, getHelloWord);
+}
+ 
+// Manage not found URL
+void handleNotFound() {
+  String message = "File Not Found\n\n";
+  message += "URI: ";
+  message += server.uri();
+  message += "\nMethod: ";
+  message += (server.method() == HTTP_GET) ? "GET" : "POST";
+  message += "\nArguments: ";
+  message += server.args();
+  message += "\n";
+  for (uint8_t i = 0; i < server.args(); i++) {
+    message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
+  }
+  server.send(404, "text/plain", message);
+}
+ 
+void setup(void) {
+  Serial.begin(115200);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.println("");
+ 
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(WIFI_SSID);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+ 
+  // Activate mDNS this is used to be able to connect to the server
+  // with local DNS hostmane esp8266.local
+  if (MDNS.begin("esp8266")) {
+    Serial.println("MDNS responder started");
+  }
+ 
+  // Set server routing
+  restServerRouting();
+  // Set not found response
+  server.onNotFound(handleNotFound);
+  // Start server
+  server.begin();
+  Serial.println("HTTP server started");
+}
+ 
+void loop(void) {
+  server.handleClient();
+}
---
+++ secrets.example.h
@@ -0,0 +1,7 @@
+#ifndef SECRETS_H
+#define SECRETS_H
+
+const char* WIFI_SSID = "<your-ssid>";
+const char* WIFI_PASSWORD = "<your-password>";
+
+#endif
