37f9fbda9812b60445d84bf9c9e91516c4f02dcc
hussein-mourad <38833034+Hussein-Mourad@users.noreply.github.com>
2024-12-01 08:50:06 +0200

    renamed secrets to config

---
+++ config.example.h
@@ -0,0 +1,6 @@
+#define BAUD_RATE 115200
+
+#define AP_SSID "<your-ssid>"
+#define AP_PASS "<your-password>"
+#define AP_TIMEOUT 120
+#define AP_TRIGGER_PIN 0 // Flash button
--- secrets.example.h
+++
@@ -1,7 +0,0 @@
-#ifndef SECRETS_H
-#define SECRETS_H
-
-const char* WIFI_SSID = "<your-ssid>";
-const char* WIFI_PASSWORD = "<your-password>";
-
-#endif
