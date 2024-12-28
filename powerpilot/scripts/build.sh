88bf8bf899688a0e1d0a6180c2e456dfe07547ed
hussein-mourad <38833034+Hussein-Mourad@users.noreply.github.com>
2024-12-28 18:32:11 +0200

    chore: add a build script
    Add a build script that injects the .env values inside the app at
    compile time

---
+++ powerpilot/scripts/build.sh
@@ -0,0 +1,14 @@
+declare module
+declare binary
+declare ldflags
+
+module="$(head go.mod -n 1 | cut -d ' ' -f2)"
+binary=powerpilot
+ldflags="-X '${module}/internal/config.BaseURL=$(printenv BASE_URL)' \
+-X '${module}/internal/config.Username=$(printenv ESP_USERNAME)' \
+-X '${module}/internal/config.Password=$(printenv ESP_PASSWORD)' \
+-X '${module}/internal/config.APSSID=$(printenv AP_USERNAME)' \
+-X '${module}/internal/config.APPASS=$(printenv AP_PASSWORD)'
+"
+
+go build -ldflags "$ldflags" -o ./tmp/$binary
